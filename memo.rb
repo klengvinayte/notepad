7c586c4e9e370ddbe24f6e676e193af540d7b520
kristina_lengvinayte <klengvinayte@gmail.com>
2022-07-15 16:53:36 +0300

    Notepad v.1 working

--- memo.rb
+++ memo.rb
@@ -1,8 +1,24 @@
 class Memo < Post
 
 def read_from_console
+	puts "Новая заметка (все, что пишите до строчки \'end\'): "
+
+	@text = []
+	line = nil
+
+	while line != "end" do
+		line = STDIN.gets.chomp
+		@text << line
 	end
 
+	@text.pop
+end
+
 	def to_strings
+
+		time_string =  "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")}  \n\r \n\r"
+        
+        #Добавляет строчку в начало массива
+		return @text.unshift(time_string)
 	end
 end
--- task.rb
+++ task.rb
@@ -1,3 +1,6 @@
+#подключаем библиотеку, чтобы приобразовывать строки к датам
+require 'date'
+
 class Task < Post
 
 	def initialize
@@ -7,8 +10,23 @@ class Task < Post
 	end
 
 	def read_from_console
+		puts "Что нужно сделать? "
+		@text = STDIN.gets.chomp
+
+		puts "К какому числу? Укажите дату в формате ДД.ММ.ГГГГ Например 15.08.2022"
+		input = STDIN.gets.chomp
+
+		# Вызываем статический метод parse на переменную input
+		@due_date = Date.parse(input)
 	end
 
 	def to_strings
+
+		time_string =  "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")}  \n\r \n\r"
+
+		deadline = "Крайний срок: #{@due_date}"
+
+		return [deadline, @text, time_string]
+
 	end
 end
